20a08c5ce4f5b351ba6e3d921054f94874ae08fc
BraxtonDParnell <braxtondparnell@gmail.com>
2024-11-05 05:33:04 -0700

    Update code.js

--- code.js
+++ code.js
@@ -4,7 +4,7 @@ function convertToAdjList(adjMatrix) {
 		for (let j = 0; j < adjMatrix[i].length; j++) {
 			if (adjMatrix[i][j] == 1) { //if there is an edge
                 if (!AdjList[i]) { 		//adds the array for edges if the current vertex has an edge and there isn't an array yet.
-										//we only need an arry if edges exist for a vertex, don't add an array for a vertex with no edges.
+						//we only need an arry if edges exist for a vertex, don't add an array for a vertex with no edges.
                     AdjList[i] = [];
                 }
                 AdjList[i].push(j);		//adds the edge
